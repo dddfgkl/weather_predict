595fe26a684acefb6ec79c1a3795f1d8cf7f52d8
cong ma <congma@congdeMacBook-Pro.local>
2019-10-07 14:25:56 +0800

    add interpolation v1  2019.10.07

--- src/interpolation.py
+++ src/interpolation.py
@@ -1,4 +1,6 @@
 import os
+import h5py
+import numpy as np
 import read_ctl
 
 # bin_file_path = "/home/datanfs/macong_data/IAP41_Hindcast_SEasian_daily_Tmax_ens_mean_87x54x180x32.bin"
@@ -33,9 +35,72 @@ def bilinear_interpolation(x, y, points):
             q22 * (x - x1) * (y - y1)
            ) / ((x2 - x1) * (y2 - y1) + 0.0)
 
-def extract_data_from_h5():
+
+def extract_data_from_h5(cpc_data, cpc_longitude, cpc_latitude):
+    # cpc_data and bin_data should be trim
+    # the core of the extraction, find the axis
+    """
+    :param cpc_data: 720x160x365x32(longitude x latitude x day x year)
+    # :param bin_data: 87x54x180x32(longitude x latitude x day x year)
+    :param cpc_longitude: longitude 720
+    :param cpc_latitude: latitude 87
+    :return:
+    """
+    cpc_x = 0
+    cpc_y = 0
+
+    longitude = 59.0625 - 1.40625
+    latitude = -14.88189 - 1.417
+    extract_data = []
+    for i in range(87):
+        longitude += 1.40625
+        latitude_array = []
+        for j in range(54):
+            latitude += 1.417
+            while cpc_x < len(cpc_longitude) and cpc_longitude[cpc_x] < longitude:
+                cpc_x += 1
+            while cpc_y < len(cpc_latitude) and cpc_latitude[cpc_y] < latitude:
+                cpc_y += 1
+            d_array = []
+            for d in range(180):
+                y_array = []
+                for y in range(32):
+                    points = ((cpc_longitude[cpc_x-1], cpc_latitude[cpc_y-1], cpc_data[cpc_x-1][cpc_y-1][d][y]),
+                              (cpc_longitude[cpc_x-1], cpc_latitude[cpc_y], cpc_data[cpc_x-1][cpc_y][d][y]),
+                              (cpc_longitude[cpc_x], cpc_latitude[cpc_y-1], cpc_data[cpc_x][cpc_y-1][d][y]),
+                              (cpc_longitude[cpc_x], cpc_latitude[cpc_y], cpc_data[cpc_x][cpc_y][d][y]))
+                    inter_value = bilinear_interpolation(longitude, latitude, points)
+                    y_array.append(inter_value)
+                d_array.append(y_array)
+            latitude_array.append(d_array)
+        extract_data.append(latitude_array)
+    extract_data = np.array(extract_data)
+    extract_data = extract_data.transpose(3, 2, 1, 0)
+    return extract_data
+
+
+def read_data_from_file(h5_file_path, store_file_path):
+    # cpc row h5 file
+    if os.path.exists(h5_file_path) == False:
+        raise FileExistsError
+    f = h5py.File(h5_file_path, 'r')
+    f_store = h5py.File(store_file_path, 'w')
+    print(f.keys())
+    for key in f.keys():
+        print(key, f[key].shape)
+    cpc_data = f["tmax"]
+    cpc_longitude = f["lon"]
+    cpc_latitude = f["lat"]
+    extract_data = extract_data_from_h5(cpc_data, cpc_longitude, cpc_latitude)
+    f_store["cpc"] = extract_data
+    f_store.close()
+    print("desc over all keys")
+
+    # bin label data
+    """
     bin_data = read_ctl.read_bin_to_numpy()
     print("data shape, ", bin_data.shape)
+    """
 
 if __name__ == '__main__':
     extract_data_from_h5()
