36db9d1740e90f469744943dd38da58e6214ee9b
macong <macong.ucaser@gmail.com>
2019-10-17 09:25:48 +0800

    modify read_nv

--- src/read_nc.py
+++ src/read_nc.py
@@ -152,7 +152,7 @@ def unit_test():
     # extract_year_from_nc_to_h5(dir_path, store_path2)'
     # desc_single_h5_file(store_file_path3)
     all_data = desc_all_ncFile(fixed_cpc_nc_dir_path)
-    store_data_2_h5(trim_cpc_nc_dir_path, all_data)
+    # store_data_2_h5(trim_cpc_nc_dir_path, all_data)
     print("unit test over!")
 
 def main():
