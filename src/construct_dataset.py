64422c06a95fd2aadfbced8d787bf0bd6026b731
macong <macong.ucaser@gmail.com>
2019-11-05 15:03:25 +0800

    psuh

--- src/construct_dataset.py
+++ src/construct_dataset.py
@@ -83,8 +83,8 @@ def construct_data(window = 6):
     print(raw_data[:][:][0][0])
     # transpose the data to the shape you want
     # present shape is 87x54x180x32
-    raw_cpc = raw_cpc.transpose(3,2,0,1)
-    raw_data = raw_data.transpose(1,0,2,3)
+    raw_cpc = raw_cpc.transpose(3,2,1,0)
+    # raw_data = raw_data.transpose(1,0,2,3)
     print(raw_cpc.shape)
     print(raw_data.shape)
     return
