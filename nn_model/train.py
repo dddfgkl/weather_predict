1ea05a32c7ef87d95a3a0a55f599fc0f8ecc8534
macong <macong.ucaser@gmail.com>
2019-11-27 21:56:15 +0800

    add

--- nn_model/train.py
+++ nn_model/train.py
@@ -28,7 +28,7 @@ torch.manual_seed(2018)
 torch.cuda.manual_seed(2018)
 
 from airConvlstm import *
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 #device = torch.device('cpu')
