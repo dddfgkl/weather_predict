92d1e4fbe948448f69a70cd010ee506deb70117f
macong <macong.ucaser@gmail.com>
2019-12-03 15:38:03 +0800

    add

--- matplot/basemap_demo.py
+++ matplot/basemap_demo.py
@@ -1,9 +1,20 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
+import netCDF4 as nc
 
 def demo1():
     # 新建地图
-    map = Basemap() #Basemap类有很多属性，这里全都使用默认参数
+    basic_nc_file = "/home/datanfs/liutao_backup1/Hind3_label/Tmax/tmax_lbl.1981.nc"
+    plt.figure(figsize=(16, 16), dpi=200, )
+    nc_data = nc.Dataset(basic_nc_file)
+    lons = nc_data.variables['lon2d'][:][0, :, :]
+    lats = nc_data.variables['lat2d'][:][0, :, :]
+
+    lon_0 = lons.mean()
+    lat_0 = lats.mean()
+    output_data = nc_data.variables['pm25'][:][0, :, :]
+
+    map = Basemap(lat_0=lat_0, lon_0=lon_0, projection='lcc', ) #Basemap类有很多属性，这里全都使用默认参数
 
     # 画图
     map.drawcoastlines()
