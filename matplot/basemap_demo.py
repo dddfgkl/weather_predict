8dd1c8aab354172f42fac73ff24bb22ee9b0f0fa
macong <macong.ucaser@gmail.com>
2019-12-03 15:54:55 +0800

    1

--- matplot/basemap_demo.py
+++ matplot/basemap_demo.py
@@ -1,20 +1,24 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import netCDF4 as nc
+import numpy as np
 
 def demo1():
     # 新建地图
     basic_nc_file = "/home/datanfs/liutao_backup1/Hind3_label/Tmax/tmax_lbl.1981.nc"
     plt.figure(figsize=(16, 16), dpi=200, )
     nc_data = nc.Dataset(basic_nc_file)
-    for x in nc_data.variables.keys():
-        print(x)
+    keys =  nc_data.variables.keys()
+    for x in keys:
+        tmp = np.array(nc_data.variables[x][:])
+        print(tmp.shape)
+        print(x, )
     lons = nc_data.variables['lon'][:]
     lats = nc_data.variables['lat'][:]
 
     lon_0 = lons.mean()
     lat_0 = lats.mean()
-    output_data = nc_data.variables['pm25'][:][0, :, :]
+    output_data = nc_data.variables['tmax'][:][0]
 
     map = Basemap(lat_0=lat_0, lon_0=lon_0, projection='lcc' ) #Basemap类有很多属性，这里全都使用默认参数
 
